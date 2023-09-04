091acbbb4f4987246c4e21a982d37fc0928356e0
mark-bromell <33425418+mark-bromell@users.noreply.github.com>
2023-09-05 00:42:43 +0100

    Added window management

--- keyboards/gmmk/pro/rev1/ansi/keymaps/mark-bromell/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/mark-bromell/keymap.c
@@ -17,61 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#include "alias.h"
 #include "features/custom_shift_keys.h"
+#include "features/cross_platform.h"
 
 #pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
 
-enum custom_keycodes {
-    MAC = SAFE_RANGE,
-    CTL_CMD, // Ctrl or Cmd
-    CTL_OPT, // Ctrl or Opt
-
-    ARROW,
-    EARROW,
-    UPDIR,
-};
-
-#define xxxxxxx = _______ // Not free for mapping for ergonomic or logical reasons
-
-// Window Management
-#define ALT_TAB = LALT(KC_TAB)
-// MacOS
-#define MV_MAX = LCA(KC_ENTER)     // Maximize window
-#define MV_BAK = LCA(KC_BACKSPACE) // Undo window change
-#define MV_SPL = LCA(KC_LEFT)      // Split left
-#define MV_SPR = LCA(KC_RIGHT)     // Split right
-#define MV_BIG = LCA(KC_EQUAL)     // Make window bigger
-#define MV_SML = LCA(KC_MINUS)     // Make window smaller
-#define MV_THL = LCA(KC_G)         // 2 thirds on the left
-#define MV_THR = LCA(KC_H)         // 1 third on the right
-#define MV_TOP = KC_LSFT
-#define MV_BOT = KC_LGUI
-#define MV_VEW = KC_MCTL           // Mission control
-#define MV_LWS = LCTL(KC_LEFT)     // Previous workspace
-#define MV_RWS = LCTL(KC_RIGHT)    // Next workspace
-// Windows
-#define W_LWS = LGUI(LCTL(KC_LEFT))
-#define W_RWS = LGUI(LCTL(KC_RIGHT))\
-
-
-// User config
-typedef union {
-    uint32_t raw;
-    struct {
-        bool macos :1;
-    };
-} user_config_t;
-user_config_t user_config;
-
-
 // clang-format off
 #define BASE_LAYOUT = LAYOUT( \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          KC_MUTE, \
-        KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,  KC_DEL,  KC_BSPC,          _______, \
-        KC_LCTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL, KC_TAB,  ALT_TAB,          _______, \
-        MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_DQUO, MO(3),            KC_ENT,           MV_BIG, \
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,         KC_RSFT, MV_MAX,  MV_SML, \
-        KC_LALT, _______, KC_LGUI,                            KC_SPC,                             KC_RGUI, _______, KC_RALT, MV_SPL,  MV_BAK,  MV_SPR \
+        MV_FULL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, QK_BOOT,          KC_MUTE, \
+        KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,  KC_DEL,  KC_BSPC,          NO_IMPL, \
+        KC_LCTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL, KC_TAB,  ALT_TAB,          NO_IMPL, \
+        MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_DQUO, MO(3),            KC_ENT,           NO_IMPL, \
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,         KC_RSFT, NO_IMPL, NO_IMPL, \
+        KC_LALT, WINDOWS, KC_LGUI,                            KC_SPC,                             KC_RGUI, MAC,     KC_RALT, NO_IMPL, NO_IMPL, NO_IMPL \
     )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -81,94 +40,215 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Right symbols
     [0] = BASE_LAYOUT,
     [1] = LAYOUT(
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, QK_RBT,           xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          _______,
-        KC_TRNS, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, _______, KC_LCBR, KC_RCBR, KC_PIPE, KC_BSLS, KC_RALT, KC_TRNS, MV_VEW,           _______,
-        xxxxxxx, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, KC_HASH, KC_LPRN, KC_RPRN, KC_COLN, KC_ESC,  _______,          _______,          _______,
-        KC_TRNS,          CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, KC_PERC, KC_DLR,  ARROW,   EARROW,  _______,          MV_RWS,  KC_LSFT, _______,
-        KC_TRNS, xxxxxxx, MO(5),                              _______,                            xxxxxxx, xxxxxxx, xxxxxxx, MV_THL,  KC_LGUI, MV_THR
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_RBT,           XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  CTL_OPT,          MV_2TL,
+        KC_TRNS, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, NO_IMPL, KC_LCBR, KC_RCBR, KC_PIPE, KC_BSLS, KC_RALT, KC_TRNS, MV_ALL,           MV_1TR,
+        _______, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, KC_HASH, KC_LPRN, KC_RPRN, KC_COLN, KC_ESC,  NO_IMPL,          NO_IMPL,          MV_TOPR,
+        KC_TRNS,          CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, KC_PERC, KC_DLR,  ARROW,   EARROW,  NO_IMPL,          MV_WSR,  MV_MAX,  MV_BOTR,
+        KC_TRNS, XXXXXXX, MO(5),                              NO_IMPL,                            XXXXXXX, XXXXXXX, XXXXXXX, MV_L,    MV_RSET, MV_R
     ),
 
     // Left symbols
     [2] = BASE_LAYOUT,
     [3] = LAYOUT(
-        _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        _______, KC_CIRC, KC_PLUS, KC_LBRC, KC_RBRC, KC_AT,   CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, KC_TRNS, xxxxxxx, xxxxxxx,          xxxxxxx,
-        _______, KC_MINS, KC_EQL,  KC_ASTR, KC_UNDS, KC_AMPR, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, xxxxxxx,          xxxxxxx,          xxxxxxx,
-        MV_LWS,           KC_TILD, KC_EXLM, KC_LABK, KC_RABK, UPDIR,   CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD,          KC_TRNS, xxxxxxx, xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx,                            _______,                            _______, xxxxxxx, KC_TRNS, xxxxxxx, xxxxxxx, xxxxxxx
+        NO_IMPL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        NO_IMPL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        NO_IMPL, KC_CIRC, KC_PLUS, KC_LBRC, KC_RBRC, KC_AT,   CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, KC_TRNS, XXXXXXX, XXXXXXX,          XXXXXXX,
+        NO_IMPL, KC_MINS, KC_EQL,  KC_ASTR, KC_UNDS, KC_AMPR, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, _______,          XXXXXXX,          XXXXXXX,
+        MV_WSL,           KC_TILD, KC_EXLM, KC_LABK, KC_RABK, UPDIR,   CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD, CTL_CMD,          KC_TRNS, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX,                            NO_IMPL,                            NO_IMPL, XXXXXXX, KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     // Navigation
     [4] = LAYOUT(
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, _______, KC_PGUP, KC_PGDN, _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, xxxxxxx, xxxxxxx,          xxxxxxx,          xxxxxxx,
-        KC_LSFT,          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_HOME, KC_LEFT, KC_RGHT, KC_END,  xxxxxxx, xxxxxxx,          xxxxxxx, xxxxxxx, xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx,                            _______,                            xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NO_IMPL, KC_PGUP, KC_PGDN, NO_IMPL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,          XXXXXXX,          XXXXXXX,
+        KC_LSFT,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_LEFT, KC_RGHT, KC_END,  XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, _______,                            NO_IMPL,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [5] = LAYOUT(
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,          xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, xxxxxxx, xxxxxxx,          xxxxxxx,          xxxxxxx,
-        KC_TRNS,          xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_TRNS, CTL_OPT, CTL_OPT, KC_TRNS, xxxxxxx, xxxxxxx,          xxxxxxx, xxxxxxx, xxxxxxx,
-        xxxxxxx, xxxxxxx, xxxxxxx,                            KC_TRNS,                            xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX,          XXXXXXX,          XXXXXXX,
+        KC_TRNS,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, CTL_OPT, CTL_OPT, KC_TRNS, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, _______,                            KC_TRNS,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 };
 // clang-format on
 
+
+user_config_t user_config = union {
+    uint32_t raw;
+    struct {
+        bool macos: 1;
+    }
+};
+
 const custom_shift_key_t custom_shift_keys[] = {
-    {KC_DQUO, KC_SEMICOLON},
-    {KC_COMMA, KC_QUOTE},
-    {KC_DOT, KC_GRAVE},
+    {KC_DQUO, KC_QUOT},
 };
 uint8_t NUM_CUSTOM_SHIFT_KEYS = sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
+
+bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
+uint16_t alt_tab_timer = 0;     // we will be using them soon.
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_custom_shift_keys(keycode, record)) { return false; }
     switch (keycode) {
-        case MAC: // MacOS toggle
+        case ARROW:
             if(record->event.pressed) {
-                user_config.macos ^= 1;
+                SEND_STRING("->");
+            }
+            break;
+        case EARROW:
+            if(record->event.pressed) {
+                SEND_STRING("=>");
+            }
+            break;
+        case UPDIR:
+            if(record->event.pressed) {
+                SEND_STRING("../");
+            }
+            break;
+
+        case ALT_TAB:
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                    register_code(KC_LALT);
+                }
+                alt_tab_timer = timer_read();
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            break;
+        case WINDOWS:
+            if(record->event.pressed) {
+                user_config.macos = 0;
                 eeconfig_update_user(user_config.raw);
             }
-            return false;
             break;
-
+        case MAC:
+            if(record->event.pressed) {
+                user_config.macos = 1;
+                eeconfig_update_user(user_config.raw);
+            }
+            break;
         case CTL_CMD:
-            if(record->event.pressed) {
-                register_code16((user_config.macos) ? LGUI(KC_TRNS) : LCTL(KC_TRNS));
-            } else {
-                unregister_code16((user_config.macos) ? LGUI(KC_TRNS) : LCTL(KC_TRNS));
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LGUI(KC_TRNS) : LCTL(KC_TRNS));
             }
-            return false;
             break;
-
         case CTL_OPT:
-            if(record->event.pressed) {
-                register_code16((user_config.macos) ? LOPT(KC_TRNS) : LCTL(KC_TRNS));
-            } else {
-                unregister_code16((user_config.macos) ? LOPT(KC_TRNS) : LCTL(KC_TRNS));
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LALT(KC_TRNS) : LCTL(KC_TRNS));
             }
-            return false;
             break;
-
-        case ARROW:
-            SEND_STRING("->");
+        case MV_FULL_TOGGLE:
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LGUI(LCTL(KC_F)) : LGUI(LCTL(KC_F)));
+            }
             break;
-
-        case EARROW:
-            SEND_STRING("=>");
+        case MV_MAX_SIZE:
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LCA(KC_ENTER) : LGUI(KC_UP));
+            }
             break;
-
-        case UPDIR:
-            SEND_STRING("../")
+        case MV_RESET_SIZE:
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LCA(KC_BACKSPACE) : LGUI(KC_DOWN));
+            }
+            break;
+        case MV_SPLIT_LEFT:
+            if (record->event.pressed) {
+                if (user_config.macos) {
+                    tap_code16(LCA(KC_LEFT));
+                } else {
+                    tap_code16_delay(LCAG(KC_0), 25);
+                    tap_code16(LGUI(KC_LEFT));
+                }
+            }
+            break;
+        case MV_SPLIT_RIGHT:
+            if (record->event.pressed) {
+                if (user_config.macos) {
+                    tap_code16(LCA(KC_RIGHT));
+                } else {
+                    tap_code16_delay(LCAG(KC_0), 25);
+                    tap_code16(LGUI(KC_RIGHT));
+                }
+            }
+            break;
+        case MV_2THIRDS_LEFT:
+            if (record->event.pressed) {
+                if (user_config.macos) {
+                    tap_code16(LCA(KC_G));
+                } else {
+                    tap_code16_delay(LCAG(KC_1), 25);
+                    tap_code16(LGUI(KC_LEFT));
+                }
+            }
+            break;
+        case MV_1THIRDS_RIGHT:
+            if (record->event.pressed) {
+                if (user_config.macos) {
+                    tap_code16(LCA(KC_H));
+                } else {
+                    tap_code16_delay(LCAG(KC_1), 25);
+                    tap_code16(LGUI(KC_RIGHT));
+                }
+            }
+            break;
+        case MV_TOP_RIGHT:
+            if (record->event.pressed) {
+                if (user_config.macos) {
+                    tap_code16(MEH(KC_H));
+                } else {
+                    tap_code16_delay(LCAG(KC_2), 25);
+                    tap_code16(LGUI(KC_RIGHT));
+                }
+            }
+            break;
+        case MV_BOTTOM_RIGHT:
+            if (record->event.pressed) {
+                if (user_config.macos) {
+                    tap_code16(LCAG(KC_H));
+                } else {
+                    tap_code16_delay(LCAG(KC_2), 25);
+                    tap_code16(LGUI(KC_RIGHT));
+                }
+            }
+            break;
+        case MV_ALL:
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? KC_MCTL : LGUI(KC_TAB));
+            }
+            break;
+        case MV_LEFT_WORKSPACE:
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LCTL(KC_LEFT) : LGUI(LCTL(KC_LEFT)));
+            }
+            break;
+        case MV_RIGHT_WORKSPACE:
+            if (record->event.pressed) {
+                tap_code16((user_config.macos) ? LCTL(KC_RIGHT) : LGUI(LCTL(KC_RIGHT)));
+            }
             break;
     }
-  return true;
+    return true;
+}
+
+void matrix_scan_user(void) {
+    if (is_alt_tab_active) {
+        if (timer_elapsed(alt_tab_timer) > 1000) {
+            unregister_code(KC_LALT);
+            is_alt_tab_active = false;
+        }
+    }
 }
 
 #if defined(ENCODER_MAP_ENABLE)
--- keyboards/gmmk/pro/rev1/ansi/keymaps/mark-bromell/rules.mk
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/mark-bromell/rules.mk
@@ -1,3 +1,4 @@
 SRC += features/custom_shift_keys.c
+SRC += features/cross_platform_macros.c
 ENCODER_MAP_ENABLE = yes
 NKRO_ENABLE = no
