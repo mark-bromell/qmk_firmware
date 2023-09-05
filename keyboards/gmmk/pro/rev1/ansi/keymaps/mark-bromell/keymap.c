ab69c97f26f521007eeed1046bbb3c7b58363913
mark-bromell <33425418+mark-bromell@users.noreply.github.com>
2023-09-05 23:44:56 +0100

    initial implementation

--- keyboards/gmmk/pro/rev1/ansi/keymaps/mark-bromell/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/mark-bromell/keymap.c
@@ -20,17 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "alias.h"
 #include "features/custom_shift_keys.h"
 
-#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
-
 // clang-format off
-#define BASE_LAYOUT = LAYOUT( \
+#define BASE_LAYOUT LAYOUT( \
         MV_FULL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, NO_IMPL, QK_BOOT,          KC_MUTE, \
         KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,  KC_DEL,  KC_BSPC,          NO_IMPL, \
         KC_LCTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    CTL_ESC, KC_TAB,  ALT_TAB,          NO_IMPL, \
         MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_DQUO, MO(3),            KC_ENT,           NO_IMPL, \
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,         KC_RSFT, KC_UP,   NO_IMPL, \
         KC_LALT, WINDOWS, KC_LGUI,                            KC_SPC,                             KC_RGUI, MAC,     KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT \
-    )
+)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // To put the keyboard into boot loader mode, press the key directly to the left of the rotary knob.
@@ -79,20 +77,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 
-user_config_t user_config = union {
+typedef union {
     uint32_t raw;
     struct {
         bool macos: 1;
-    }
-};
+    };
+} user_config_t;
+user_config_t user_config;
 
 const custom_shift_key_t custom_shift_keys[] = {
     {KC_DQUO, KC_QUOT},
 };
 uint8_t NUM_CUSTOM_SHIFT_KEYS = sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
 
-bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
-uint16_t alt_tab_timer = 0;     // we will be using them soon.
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_custom_shift_keys(keycode, record)) { return false; }
@@ -260,6 +260,10 @@ void matrix_scan_user(void) {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [4] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [5] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
